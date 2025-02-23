cdfa205c3d9ff1d8577c66b4650ecfba60829d54
huangweizhe <naruto60913@gmail.com>
2025-02-23 12:19:37 +0800

    改為mvvm

--- app/src/main/java/com/huangliner/mqtttest/MainActivity.kt
+++ app/src/main/java/com/huangliner/mqtttest/MainActivity.kt
@@ -1,22 +1,20 @@
 package com.huangliner.mqtttest
 
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import com.huangliner.mqtttest.Utils.Companion.disableView
+import com.huangliner.mqtttest.Utils.Companion.enableView
 import com.huangliner.mqtttest.databinding.ActivityMainBinding
 import com.huangliner.mqtttest.viewmodel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import info.mqtt.android.service.MqttAndroidClient
-import org.eclipse.paho.client.mqttv3.IMqttActionListener
-import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken
-import org.eclipse.paho.client.mqttv3.IMqttToken
-import org.eclipse.paho.client.mqttv3.MqttCallback
-import org.eclipse.paho.client.mqttv3.MqttConnectOptions
-import org.eclipse.paho.client.mqttv3.MqttException
-import org.eclipse.paho.client.mqttv3.MqttMessage
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import timber.log.Timber
 
 @AndroidEntryPoint
@@ -26,101 +24,94 @@ class MainActivity : AppCompatActivity() {
     private val mqttIP = "tcp://broker.emqx.io:1883"
     private val mqttClientID = "223"
     private val viewModel by viewModels<MainViewModel>()
-    var isConnect:Boolean = false
-    private lateinit var mqttAndroidClient: MqttAndroidClient
+    private var isConnect:Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Timber.plant(Timber.DebugTree())
         enableEdgeToEdge()
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.viewMode = viewModel
         binding.btnMainConnect.setOnClickListener {
             if(isConnect){
-                try{
-                    mqttAndroidClient.disconnect()
-                    binding.btnMainConnect.text = "連線"
-
-                }catch (e:Exception){
-                    Timber.e("測試 錯誤 ${Log.getStackTraceString(e)}")
-                }
+                viewModel.disconnectMqtt()
             }else {
-                try {
-                    Timber.d("測試 連線")
-                    mqttAndroidClient = MqttAndroidClient(this, mqttIP, mqttClientID)
-                    val mqttAction = MqttConnectOptions()
-                    mqttAction.connectionTimeout = 30
-                    mqttAction.keepAliveInterval = 120
-                    mqttAction.isAutomaticReconnect = true
-                    mqttAction.isCleanSession = true
-                    mqttAndroidClient.setCallback(object:MqttCallback{
-                        override fun connectionLost(cause: Throwable?) {
-                            cause?.let {
-                                Timber.e("測試 丟失 ${it.message}")
-                            }
-                        }
-
-                        override fun messageArrived(topic: String?, message: MqttMessage?) {
-                            Timber.e("測試 接收到${topic}")
-                            val data = String(message?.payload!!)
-                            Timber.e("測試 接收到${data}")
-                        }
-
-                        override fun deliveryComplete(token: IMqttDeliveryToken?) {
-                            token?.let {
-                                Timber.d("測試 完整接收 ${token.message}")
-                            }
-                        }
-                    })
-                    val token = mqttAndroidClient.connect(mqttAction)
-                    token.actionCallback = object : IMqttActionListener {
-                        override fun onSuccess(asyncActionToken: IMqttToken?) {
-                            Timber.d("測試 Mqtt 連線成功")
-                        }
-
-                        override fun onFailure(
-                            asyncActionToken: IMqttToken?,
-                            exception: Throwable?
-                        ) {
-                            Timber.d("測試 Mqtt 連線失敗 ${Log.getStackTraceString(exception)}")
-                        }
-
-                    }
-
-                    binding.btnMainConnect.text = "中斷連線"
-                } catch (e: MqttException) {
-                    e.printStackTrace()
-                }
+                viewModel.connectMqtt(mqttIP = mqttIP, mqttClientID = mqttClientID)
             }
-
-            isConnect = !isConnect
         }
 
         binding.btnMainSubscribe.setOnClickListener {
             val topic = binding.editMainTopic.text.toString()
-            Timber.d("按下訂閱 : $topic")
-            if(topic.isEmpty()){
-                Toast.makeText(this,"請輸入訂閱主題",Toast.LENGTH_SHORT).show()
-                return@setOnClickListener
-            }
-            val token = mqttAndroidClient.subscribe(topic,0)
-            token.actionCallback = object : IMqttActionListener{
-                override fun onSuccess(asyncActionToken: IMqttToken?) {
-                    Timber.d("訂閱${topic}成功")
-                }
-
-                override fun onFailure(asyncActionToken: IMqttToken?, exception: Throwable?) {
-                    Timber.e("訂閱${topic}失敗")
-                }
-
-            }
+            viewModel.subscript(topic)
         }
 
         binding.btnMainEmit.setOnClickListener {
             val topic = binding.editMainTopic.text.toString()
-            val mqttMessage = MqttMessage("123".toByteArray()).apply {
-                qos = 1 // 設置 QoS
+            val message = binding.editMainMessage.text.toString()
+            viewModel.emitMessageToBroker(message = message,topic = topic)
+        }
+
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED){
+                viewModel.emitState.collectLatest {
+                    if(it.isNotEmpty()){
+                        Toast.makeText(this@MainActivity,it,Toast.LENGTH_SHORT).show()
+                    }
+                }
             }
-            mqttAndroidClient.publish(topic, mqttMessage)
+        }
+
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED){
+                viewModel.subscriptState.collectLatest {
+                    if(it.isNotEmpty()){
+                        Toast.makeText(this@MainActivity,it,Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED){
+                viewModel.mqttMessage.collectLatest {
+                    Timber.e("測試 view 收到的消息${it}")
+                }
+            }
+        }
+
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED){
+                viewModel.connectState.collect{
+                    when(it){
+                        is MqttConnectState.Error -> {
+                            binding.btnMainConnect.text = "連線錯誤，請確認網路狀況"
+                            binding.btnMainConnect.enableView()
+                            binding.btnMainSubscribe.disableView()
+                            binding.btnMainEmit.disableView()
+                        }
+                        is MqttConnectState.Idle -> {
+                            binding.btnMainConnect.text = "連線"
+                            binding.btnMainConnect.enableView()
+                            binding.btnMainSubscribe.disableView()
+                            binding.btnMainEmit.disableView()
+                        }
+                        is MqttConnectState.Loading -> {
+                            binding.btnMainConnect.text = "連線中"
+                            binding.btnMainConnect.disableView()
+                            binding.btnMainSubscribe.disableView()
+                            binding.btnMainEmit.disableView()
+                        }
+                        is MqttConnectState.Success -> {
+                            binding.btnMainConnect.text = "中斷連線"
+                            binding.btnMainConnect.enableView()
+                            binding.btnMainSubscribe.enableView()
+                            binding.btnMainEmit.enableView()
+                        }
+                    }
+                }
+
+            }
         }
     }
 
--- app/src/main/java/com/huangliner/mqtttest/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/huangliner/mqtttest/viewmodel/MainViewModel.kt
@@ -1,11 +1,138 @@
 package com.huangliner.mqtttest.viewmodel
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import com.huangliner.mqtttest.MqttConnectState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import info.mqtt.android.service.MqttAndroidClient
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import org.eclipse.paho.client.mqttv3.IMqttActionListener
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken
+import org.eclipse.paho.client.mqttv3.IMqttToken
+import org.eclipse.paho.client.mqttv3.MqttCallback
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions
+import org.eclipse.paho.client.mqttv3.MqttException
+import org.eclipse.paho.client.mqttv3.MqttMessage
 import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor(): ViewModel() {
+class MainViewModel @Inject constructor(private val application: Application) :
+    AndroidViewModel(application) {
+    private val _connectState = MutableStateFlow<MqttConnectState<String>>(MqttConnectState.Idle())
+    private val _mqttMessage = MutableStateFlow("")
+    private val _subscriptState = MutableStateFlow("")  //如果空字串代表成功，不是空字串帶有錯誤訊息
+    private val _emitState = MutableStateFlow("")
+    val connectState = _connectState.asStateFlow()
+    val mqttMessage = _mqttMessage.asStateFlow()
+    val subscriptState = _subscriptState.asStateFlow()
+    val emitState = _emitState.asStateFlow()
+    private lateinit var mqttAndroidClient: MqttAndroidClient
 
+    fun connectMqtt(mqttIP: String, mqttClientID: String) {
+        _connectState.value = MqttConnectState.Loading()
+        try {
+            Timber.d("測試 連線")
+            mqttAndroidClient = MqttAndroidClient(application, mqttIP, mqttClientID)
+            val mqttAction = MqttConnectOptions()
+            mqttAction.connectionTimeout = 30
+            mqttAction.keepAliveInterval = 120
+            mqttAction.isAutomaticReconnect = true
+            mqttAction.isCleanSession = true
+            mqttAndroidClient.setCallback(object : MqttCallback {
+                override fun connectionLost(cause: Throwable?) {
+                    cause?.let {
+                        Timber.e("測試 丟失 ${it.message}")
+                    }
+                }
+
+                override fun messageArrived(topic: String?, message: MqttMessage?) {
+                    val data = String(message?.payload!!)
+                    _mqttMessage.value = data
+                    Timber.e("測試 接收到${topic}")
+                    Timber.e("測試 接收到${data}")
+                }
+
+                override fun deliveryComplete(token: IMqttDeliveryToken?) {
+                    token?.let {
+                        Timber.d("測試 完整發送 ${token.message}")
+                    }
+                }
+            })
+            val token = mqttAndroidClient.connect(mqttAction)
+            token.actionCallback = object : IMqttActionListener {
+                override fun onSuccess(asyncActionToken: IMqttToken?) {
+                    Timber.d("測試 Mqtt 連線成功")
+
+                    _connectState.value = MqttConnectState.Success()
+                }
+
+                override fun onFailure(
+                    asyncActionToken: IMqttToken?,
+                    exception: Throwable?
+                ) {
+                    _connectState.value = MqttConnectState.Error(exception?.message)
+                    Timber.d("測試 Mqtt 連線失敗 ${Log.getStackTraceString(exception)}")
+                }
+
+            }
+
+        } catch (e: MqttException) {
+            e.printStackTrace()
+        }
+    }
+
+    fun disconnectMqtt(){
+        try{
+            mqttAndroidClient.disconnect()
+
+        }catch (e:Exception){
+            Timber.e("測試 錯誤 ${Log.getStackTraceString(e)}")
+        }
+    }
+
+    fun subscript(topic:String){
+        Timber.d("按下訂閱 : $topic")
+        if(topic.isEmpty()){
+            _subscriptState.value = "不能輸入為空"
+            return
+        }
+        val token = mqttAndroidClient.subscribe(topic,0)
+        token.actionCallback = object : IMqttActionListener{
+            override fun onSuccess(asyncActionToken: IMqttToken?) {
+                Timber.d("訂閱${topic}成功")
+                _subscriptState.value = ""
+            }
+
+            override fun onFailure(asyncActionToken: IMqttToken?, exception: Throwable?) {
+                Timber.e("訂閱${topic}失敗")
+                _subscriptState.value = "訂閱${topic}失敗 ${exception?.message}"
+            }
+
+        }
+    }
+
+    fun emitMessageToBroker(
+        message:String,
+        topic:String,
+        qosLevel :Int = 0) {
+        if(message.isEmpty()){
+            _emitState.value = "請輸入內容"
+            return
+        }
+
+        if(topic.isEmpty()){
+            _emitState.value = "請輸入主題"
+            return
+        }
+        val mqttMessage = MqttMessage(message.toByteArray()).apply {
+            qos = 1 // 設置 QoS
+        }
+        mqttAndroidClient.publish(topic, mqttMessage)
+
+        _emitState.value = ""
+
+    }
 }
